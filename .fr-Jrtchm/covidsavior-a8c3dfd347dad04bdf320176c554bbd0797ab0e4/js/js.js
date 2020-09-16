36eb1cb2a56ae29a29ebabc27a0df0a30a8b2158
Sebawe <syajsebawe@gmail.com>
2020-09-16 18:28:56 +0300

    npm reinstalled

---
+++ .fr-Jrtchm/covidsavior-a8c3dfd347dad04bdf320176c554bbd0797ab0e4/js/js.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+
+var theChecker = true;
+var phoneList = document.getElementById("pH");
+function viewSearch(){
+	if(theChecker == true){
+			phoneList.setAttribute("style", "display:block");
+			theChecker= false;
+			}
+		else{
+			phoneList.setAttribute( "style", "display:none");
+			
+			theChecker= true;}
+}
+
+
+
+window.onresize = function restDefault(){
+				 hidInfo();}
+function  hidInfo(){
+	var menue = document.getElementById("pH");
+	
+	menue.style.display="none";
+	
+	}
+
